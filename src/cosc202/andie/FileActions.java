25f56073e5c8474675af0cfb643c678116dc8670
Arlo Hill <arlohill02@gmail.com>
2022-03-22 13:29:57 +1300

    added an export button along with a popup to select which export type you want to use

--- src/cosc202/andie/FileActions.java
+++ src/cosc202/andie/FileActions.java
@@ -37,6 +37,7 @@ public class FileActions {
         actions.add(new FileOpenAction("Open", null, "Open a file", Integer.valueOf(KeyEvent.VK_O)));
         actions.add(new FileSaveAction("Save", null, "Save the file", Integer.valueOf(KeyEvent.VK_S)));
         actions.add(new FileSaveAsAction("Save As", null, "Save a copy", Integer.valueOf(KeyEvent.VK_A)));
+        actions.add(new Export("Export", null, "Export file", Integer.valueOf(KeyEvent.VK_P)));
         actions.add(new FileExitAction("Exit", null, "Exit the program", Integer.valueOf(0)));
     }
 
@@ -244,6 +245,70 @@ public class FileActions {
          */
         public void actionPerformed(ActionEvent e) {
             System.exit(0);
+        }
+
+    }
+
+
+
+
+    public class Export extends ImageAction {
+
+        /**
+         * <p>
+         * Create a new file-save-as action.
+         * </p>
+         * 
+         * @param name The name of the action (ignored if null).
+         * @param icon An icon to use to represent the action (ignored if null).
+         * @param desc A brief description of the action  (ignored if null).
+         * @param mnemonic A mnemonic key to use as a shortcut  (ignored if null).
+         */
+        Export(String name, ImageIcon icon, String desc, Integer mnemonic) {
+            super(name, icon, desc, mnemonic);
+        }
+
+         /**
+         * <p>
+         * Callback for when the file-save-as action is triggered.
+         * </p>
+         * 
+         * <p>
+         * This method is called whenever the FileSaveAsAction is triggered.
+         * It prompts the user to select a file and saves the image to it.
+         * </p>
+         * 
+         * @param e The event triggering this callback.
+         */
+        public void actionPerformed(ActionEvent e) {
+            
+
+            String[] types = {".png",".jpg"};
+            
+            JComboBox exportComboBox = new JComboBox(types);
+
+            int option = JOptionPane.showOptionDialog(null, exportComboBox, "Enter export type", JOptionPane.OK_CANCEL_OPTION, JOptionPane.QUESTION_MESSAGE, null, null, null);
+
+            if (option == JOptionPane.CANCEL_OPTION) {
+                return;
+            } else if (option == JOptionPane.OK_OPTION) {
+
+                String output = ""+ exportComboBox.getItemAt(exportComboBox.getSelectedIndex());
+                
+                JFileChooser fileChooser = new JFileChooser();
+                int result = fileChooser.showOpenDialog(target);
+                System.out.println(output);
+                if (result == JFileChooser.APPROVE_OPTION) {
+                    try {
+                        String imageFilepath = fileChooser.getSelectedFile().getCanonicalPath()+output;
+                        target.getImage().saveAs(imageFilepath);
+                    } catch (Exception ex) {
+                        System.exit(1);
+                    }
+                }
+            }
+
+            
         }
 
     }
