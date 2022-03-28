fd05976dcbacfa30ac577cbc86920d957f1db90f
Jet Hughes <hugje043@student.otago.ac.nz>
2022-03-28 14:32:51 +1300

    Added PopUp class

---
+++ src/cosc202/andie/PopUp.java
@@ -0,0 +1,17 @@
+package cosc202.andie;
+
+import javax.swing.JOptionPane;
+
+/**
+ * Class used for show a messge dialog box
+ */
+public class PopUp {
+
+    /**
+     * Method to show a message box
+     * @param message String to show in the pop up box
+     */
+    public static void showMessageDialog(String message) {
+        JOptionPane.showMessageDialog(null, "ImageIO read error: Check file name and/or type");
+    }
+}
