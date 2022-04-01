f222ecab45584f21caf6132136fe5f4d2ef8d2cd
Will Murrell <murwi380@student.otago.ac.nz>
2022-04-01 16:29:29 +1300

    Fixed copy/paste error in Export documentation

--- src/cosc202/andie/FileActions.java
+++ src/cosc202/andie/FileActions.java
@@ -270,7 +270,7 @@ public class FileActions {
 
         /**
          * <p>
-         * Create a new file-save-as action.
+         * Create a new export action.
          * </p>
          * 
          * @param name The name of the action (ignored if null).
@@ -284,12 +284,12 @@ public class FileActions {
 
          /**
          * <p>
-         * Callback for when the file-save-as action is triggered.
+         * Callback for when the export action is triggered.
          * </p>
          * 
          * <p>
-         * This method is called whenever the FileSaveAsAction is triggered.
-         * It prompts the user to select a file and saves the image to it.
+         * This method is called whenever the ExportAction is triggered.
+         * It prompts the user to select a file and exports it.
          * </p>
          * 
          * @param e The event triggering this callback.
