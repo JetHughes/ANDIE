ae669cb816df612e60025baba0241ca1780b291a
Jet Hughes <hugje043@student.otago.ac.nz>
2022-04-06 14:01:28 +1200

    Added javadoc comments for the AdjustContrast class

--- src/cosc202/andie/AdjustContrast.java
+++ src/cosc202/andie/AdjustContrast.java
@@ -3,22 +3,64 @@ package cosc202.andie;
 import java.awt.image.*;
 import java.awt.Color;
 
+/** 
+ * <p>
+ * ImageOperation to adjust the contrast of an image
+ * </p>
+ * 
+*/
 public class AdjustContrast implements ImageOperation, java.io.Serializable{
+
+    /** 
+     * The percentage to increase or decrease the contrast
+    */
     private double contrast;
 
+    /**
+     * <p>
+     * Construct an AdjustContrast with the given Contrast
+     * </p>
+     * 
+     * @param contrast The percentage to increase or decrease the Contrast
+     */
     public AdjustContrast(double contrast){
         this.contrast = contrast;
-
     }
 
+    /**
+     * <p>
+     * Construct an AdjustContrast with the default Contrast
+     * </p>
+     * 
+     * <p>
+     * BY default, the percentage is 10
+     * </p>
+     * 
+     * @see AdjustContrast(int)
+     */
     public AdjustContrast(){
         this(10);
     }
 
+    //for testing
     public double returnContrast(){
         return contrast;
     }
 
+    /**
+     * <p>
+     * Apply Contrast adjustment to an image
+     * </p>
+     * 
+     * <p>
+     * The Contrast adjustment is applied using the equation 2.5 of the 202 lab book.
+     * Where b = 0 and c = contrast;
+     * The lab book is here: https://cosc202.cspages.otago.ac.nz/lab-book/COSC202LabBook.pdf
+     * </p>
+     * 
+     * @param input the image to apply the Contrast adjustment to
+     * @return The resulting adjusted image
+     */
     public BufferedImage apply(BufferedImage input) {
 
         BufferedImage output = new BufferedImage(input.getColorModel(), input.copyData(null), input.isAlphaPremultiplied(), null);
@@ -36,13 +78,18 @@ public class AdjustContrast implements ImageOperation, java.io.Serializable{
                 b = (int) getAdjustedValue(contrast, b);
 
                 output.setRGB(x, y, new Color(r,g,b).getRGB());
-
             }
         }
 
         return output;
     }
-
+    
+    /**
+     * Method to apply a Contrast adjusted to a color channel of a pixel
+     * @param b the value to adjust the Contrast by
+     * @param v the value to adjust
+     * @return The adjusted value
+     */
     public double getAdjustedValue(double c, double v) {
         double result = (1 + (c / 100)) * (v - 127.5);
 
