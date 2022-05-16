3be7af5ea83883696f3f2b919a6d97d3406f2ce6
Bradley Francis <frabr651@student.otago.ac.nz>
2022-05-17 11:02:12 +1200

    Working but buggy AreaSelect

--- src/cosc202/andie/AreaSelect.java
+++ src/cosc202/andie/AreaSelect.java
@@ -1,10 +1,72 @@
 package cosc202.andie;
 
-import javafx.scene.input.MouseEvent;
+import java.awt.event.*;
 
-public class AreaSelect {
+public class AreaSelect implements MouseListener {
     
-    AreaSelect(){
-        
+    int xOrigin, yOrigin, xEnd, yEnd;
+    boolean selected = false;
+    ImagePanel target;
+
+    public AreaSelect(ImagePanel target){
+        this.target = target;
+        target.addMouseListener(this);
     }
+
+    @Override
+    public void mousePressed(MouseEvent e) {
+        xOrigin = e.getX();
+        yOrigin = e.getY();
+        
+        // //for testing purposes to see that the location is being read properly
+        // System.out.println(xOrigin + "    " + yOrigin);
+    }
+
+    @Override
+    public void mouseReleased(MouseEvent e) {
+        xEnd = e.getX();
+        yEnd = e.getY();
+        target.removeMouseListener(this);
+
+        //makes it so that all mouse movements work
+        if(xOrigin > xEnd){
+            int n = xOrigin;
+            xOrigin = xEnd;
+            xEnd = n;
+        }
+        if(yOrigin > yEnd){
+            int n = yOrigin;
+            yOrigin = yEnd;
+            yEnd = n;
+        }
+
+        // target.getImage().apply(new DrawShapes(getXOrigin(), getYOrigin(), getXEnd(), getYEnd()));
+        // target.repaint();
+        // target.getParent().revalidate();
+    }
+
+    public int getXOrigin(){
+        return xOrigin;
+    }
+
+    public int getYOrigin(){
+        return yOrigin;
+    }
+    
+    public int getXEnd(){
+        return xEnd;
+    }
+    
+    public int getYEnd(){
+        return yEnd;
+    }
+
+    @Override
+    public void mouseClicked(MouseEvent e) {}
+
+    @Override
+    public void mouseEntered(MouseEvent e) {}
+
+    @Override
+    public void mouseExited(MouseEvent e) {}
 }
